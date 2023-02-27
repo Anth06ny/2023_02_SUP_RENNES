8bfc0d2199f753ccf7294ecad2a2a45d8a2acd77
Anthony <anth06ny@gmail.com>
2023-02-27 10:18:07 +0100

    Initial commit

---
+++ app/src/main/java/com/amonteiro/a2023_02_sup_rennes/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.amonteiro.a2023_02_sup_rennes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/amonteiro/a2023_02_sup_rennes/exoKotlin.kt
@@ -0,0 +1,5 @@
+package com.amonteiro.a2023_02_sup_rennes
+
+fun main() {
+    println("HelloWorld")
+}
