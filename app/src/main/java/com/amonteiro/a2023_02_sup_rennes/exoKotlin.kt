92ef96be0166ab226220be0a0a2fe408acc62c3b
Anthony <anth06ny@gmail.com>
2023-02-27 12:00:05 +0100

    Correction TP Function myprint et pair

--- app/src/main/java/com/amonteiro/a2023_02_sup_rennes/exoKotlin.kt
+++ app/src/main/java/com/amonteiro/a2023_02_sup_rennes/exoKotlin.kt
@@ -2,4 +2,39 @@ package com.amonteiro.a2023_02_sup_rennes
 
 fun main() {
     println("HelloWorld")
+
+//    Créer une variable v1 de type String et y mettre la chaine "toto"
+    var v1: String = "toto"
+//    Afficher dans la console sa version en majuscule (uppercase)
+    println(v1.uppercase())
+
+//    Essayer d'y mettre la valeur null. (Erreur de compilation)
+    //v1 = null
+
+//    Créer une variable v2 de type String nullable et y mettre la chaine "toto"
+    var v2: String? = "toto"
+
+//    Afficher dans la console sa version en majuscule. Obligé de mettre le safe check
+    println(v2?.uppercase())
+
+//    Créer une variable v3 de type String nullable et y mettre la valeur null
+    var v3: String? = null
+//    Afficher dans la console sa version en majuscule
+    println(v3?.uppercase())
+
+    var v4 = v3 + v3
+    println(v4)
+
+    if (v3.isNullOrBlank()) {
+        println("V3 est null ou blanc")
+    }
+
+    val res = min(1,2,3)
+    println("res= $res")
 }
+
+fun pair(c:Int) = c%2 == 0
+fun myPrint(text:String) = println("#$text#")
+
+fun min(a: Int, b: Int, c: Int) = if (a < b && a < c) a else if (b < a && b < c) b else c
+
