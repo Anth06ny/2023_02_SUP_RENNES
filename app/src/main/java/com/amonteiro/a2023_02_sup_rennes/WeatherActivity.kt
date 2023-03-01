691bb3023307b1a63aac06d86a922c983a706665
Anthony <anth06ny@gmail.com>
2023-03-01 17:17:55 +0100

    Correction TP Correction tache asynchrone

--- app/src/main/java/com/amonteiro/a2023_02_sup_rennes/WeatherActivity.kt
+++ app/src/main/java/com/amonteiro/a2023_02_sup_rennes/WeatherActivity.kt
@@ -2,6 +2,7 @@ package com.amonteiro.a2023_02_sup_rennes
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import com.amonteiro.a2023_02_sup_rennes.databinding.ActivityWeatherBinding
 import kotlin.concurrent.thread
 
@@ -13,6 +14,20 @@ class WeatherActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+        binding.btLoad.setOnClickListener {
+
+            binding.progressBar.isVisible = true
+
+            thread {
+                val weather = RequestUtils.loadWeather("Toulouse")
+
+                runOnUiThread {
+                    binding.progressBar.isVisible = false
+                    binding.tvData.text = "Il fait ${weather.main.temp}° à ${weather.name} avec un vent de ${weather.wind.speed} km/h"
+                }
+            }
+            binding.tvData.setText("En cours...")
+        }
 
     }
 }
