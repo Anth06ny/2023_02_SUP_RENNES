b6fd84302bd71c69aa34de361ce8b1d7ca9463b3
Anthony <anth06ny@gmail.com>
2023-03-01 14:54:15 +0100

    Correction TP Manip XML Partie 2

--- app/src/main/java/com/amonteiro/a2023_02_sup_rennes/MainActivity.kt
+++ app/src/main/java/com/amonteiro/a2023_02_sup_rennes/MainActivity.kt
@@ -1,11 +1,12 @@
 package com.amonteiro.a2023_02_sup_rennes
 
+import android.graphics.Color
 import android.os.Bundle
-import android.view.View
+import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
 import com.amonteiro.a2023_02_sup_rennes.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity(), View.OnClickListener {
+class MainActivity : AppCompatActivity() {
 
     //Instantiation
     val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
@@ -15,17 +16,24 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         //Affichage
         setContentView(binding.root)
 
+        //clic sur le bouton valider
         binding.btValidate.setOnClickListener {
-            println("clic")
-            binding.et.setText("Clic sur Valider")
+            if (binding.rbLike.isChecked) {
+                binding.et.setText(binding.rbLike.text)
+            }
+            else if (binding.rbDislike.isChecked) {
+                binding.et.setText(binding.rbDislike.text)
+            }
+            binding.iv.setImageResource(R.drawable.baseline_flag_24)
+            binding.iv.setColorFilter(getColor(R.color.purple_200))
         }
 
-        binding.btCancel.setOnClickListener(this)
+        binding.btCancel.setOnClickListener {
+            binding.rg.clearCheck()
+            binding.et.setText("")
+            binding.iv.setImageResource(R.drawable.baseline_delete_forever_24)
+        }
     }
 
-    override fun onClick(v: View?) {
-       if(v === binding.btCancel) {
-           binding.et.setText("Clic sur Annuler")
-       }
-    }
+
 }
