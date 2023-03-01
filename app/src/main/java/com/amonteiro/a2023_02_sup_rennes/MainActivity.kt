0efe60e512111798be9b60e0adb3485b645b6b18
Anthony <anth06ny@gmail.com>
2023-03-01 12:46:33 +0100

    Correction TP Manip XML Partie 1

--- app/src/main/java/com/amonteiro/a2023_02_sup_rennes/MainActivity.kt
+++ app/src/main/java/com/amonteiro/a2023_02_sup_rennes/MainActivity.kt
@@ -1,13 +1,31 @@
 package com.amonteiro.a2023_02_sup_rennes
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.amonteiro.a2023_02_sup_rennes.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    //Instantiation
+    val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //Affichage
+        setContentView(binding.root)
 
+        binding.btValidate.setOnClickListener {
+            println("clic")
+            binding.et.setText("Clic sur Valider")
+        }
 
+        binding.btCancel.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+       if(v === binding.btCancel) {
+           binding.et.setText("Clic sur Annuler")
+       }
     }
 }
